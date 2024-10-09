defa8f13cf8063bfa77427c2e12db2600f371950
chuong1210 <chuongbd1012@gmail.com>
2024-10-09 19:29:04 +0700

    add client

---
+++ AuthenticationClient/client/src/app/app.routes.ts
@@ -0,0 +1,36 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { AccountComponent } from './pages/account/account.component';
+import { authGuard } from './guards/auth.guard';
+import { UsersComponent } from './pages/users/users.component';
+import { roleGuard } from './guards/role.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+  {
+    path: 'account/:id',
+    component: AccountComponent,
+    canActivate: [authGuard],
+  },
+  {
+    path: 'users',
+    component: UsersComponent,
+    canActivate: [roleGuard],
+    data: {
+      roles: ['Admin'],
+    },
+  },
+];
